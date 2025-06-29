5d27e83a46431e60f78c06b92936dce7bc096614
BadoniShreyansh <badonishreyansh@gmail.com>
2025-06-29 19:01:05 +0530

    completed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
+/* eslint-disable quotes */
+/* eslint-disable implicit-arrow-linebreak */
 import { configureStore } from "@reduxjs/toolkit";
-import { shazamCoreApi } from "./services/shazamCore";
 
+import { shazamCoreApi } from "./services/shazamCore";
 import playerReducer from "./features/playerSlice";
 
 export const store = configureStore({
   reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(shazamCoreApi.middleware),
 });
